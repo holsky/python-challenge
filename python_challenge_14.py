e2850e850f1f95d8d18925d2beb07ce09b35e792
Holger Schmeisky <holger@schmeisky.com>
2014-05-17 23:09:26 +0200

    refactored challenge 14 into nice code

--- python_challenge_14.py
+++ python_challenge_14.py
@@ -1,36 +1,46 @@
 import Image
 
+def fold_spiral(move_direction, line_length):
+    global z
+    for i in xrange(line_length):
+        move_direction()
+        spiral.putpixel((x,y), wire.getpixel((z, 0)))
+        z += 1
+
+
+def move_right():
+    global x
+    x += 1
+
+def move_down():
+    global y
+    y += 1
+
+def move_left():
+    global x
+    x -= 1
+
+def move_up():
+    global y
+    y -= 1
+    
 wire = Image.open("wire.png")
 spiral = Image.new(wire.mode, (100,100))
 
-d = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
 z = 0
-x, y = (-1,0)
-for i in xrange(50, 0, -1):
-    #print "({}, {}, {}, {})".format(str(i), str(i-1), str(i-1), str(i-2))
-    for j in xrange(i + 1):
-        x += 1
-        print "({}, {})".format(str(x), str(y))
-        spiral.putpixel((x,y), wire.getpixel((z, 0)))
-        z += 1
-    for j in xrange(i):
-        print "({}, {})".format(str(x), str(y))
-        y += 1
-        spiral.putpixel((x,y), wire.getpixel((z, 0)))
-        z += 1
-    for j in xrange(i):
-        print "({}, {})".format(str(x), str(y))        
-        x -= 1
-        spiral.putpixel((x,y), wire.getpixel((z, 0)))
-        z += 1
-    for j in xrange(i - 1):
-        print "({}, {})".format(str(x), str(y))
-        y -= 1
-        spiral.putpixel((x,y), wire.getpixel((z, 0)))
-        z += 1
+x, y = (-1, 0)
+rounds = 50
+
+for round_number in xrange(rounds, 0, -1):
+    round_length = round_number * 2
+    fold_spiral(move_right, round_length)
+    fold_spiral(move_down, round_length - 1)
+    fold_spiral(move_left, round_length - 1)
+    fold_spiral(move_up, round_length - 2)
 
 spiral.save("spiral.png")
+
+
 
 
 #100+99+99+98 + (98+97+97+96) + (96 + 95 + 95 + 94)
